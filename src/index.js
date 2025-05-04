9c9c4fd6d6cee27f241532b1c61e64c58058f60e
Tonho <antonioreverso.com@gmail.com>
2025-05-04 17:17:28 -0300

    Implement web scraping functionality to extract news data from government website

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const urlpai = "https://www.gov.br/mds/pt-br/noticias-e-conteudos/noticias"
+
+function extraidados(link) {
+  axios.get(link)
+    .then(resp => {
+      const dadoshtml = resp.data
+      const $ = cheerio.load(dadoshtml)
+      const titulo = $('h1').text()
+      const linkimg = $('img').attr('src')
+      const datapublicacao = $('.value').text()
+      const texto = $('div[property="rnews:articleBody"]').text();
+      const dados = { titulo, linkimg, datapublicacao, texto }
+
+      console.log(dados)
+    })
+}
+
+const links = axios.get(urlpai)
+  .then(resp => {
+    const dadoshtml = resp.data;
+    const $ = cheerio.load(dadoshtml);
+    const noticias = []
+    $('a.summary.url')
+      .each((i, e) => {
+        const link = $(e).attr('href')
+        noticias.push(link)
+      });
+    return noticias
+  });
+
+
+async function main() {
+  const lnks = await links;
+  lnks.map((i, e) => {
+    extraidados(i);
+  })
+
+};
+
+main();
